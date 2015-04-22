70e6e177aa821c3de5cfe62607a425d349ee8933
Dan Brajkovic <dbrajkovic@mac.com>
2015-04-22 18:21:26 -0500

    Initial Commit

---
+++ Gabby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gabby
+//
+//  Created by Dan Brajkovic on 4/22/15.
+//  Copyright (c) 2015 Pragmatic Labs, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
